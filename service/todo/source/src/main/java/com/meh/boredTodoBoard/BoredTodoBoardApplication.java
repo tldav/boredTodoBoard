ffc9529027c134a9a6072835a3078e87704af0d5
Thomas Davis <tldavis09@yahoo.com>
2023-01-18 14:54:12 -0600

    add spring boot app for todo service

---
+++ service/todo/source/src/main/java/com/meh/boredTodoBoard/BoredTodoBoardApplication.java
@@ -0,0 +1,13 @@
+package com.meh.boredTodoBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoredTodoBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoredTodoBoardApplication.class, args);
+	}
+
+}
